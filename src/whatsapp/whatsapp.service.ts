2e8548221abfc7e05c45dfd1e771ca07960abcdc
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-07-22 10:46:17 -0400

    config whatsapp send message by local machine

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -1,10 +1,28 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { Client, LocalAuth } from 'whatsapp-web.js';
-import * as qrcode from 'qrcode-terminal';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+// import { Client, LocalAuth } from 'whatsapp-web.js';
+// import * as qrcode from 'qrcode-terminal';
+import axios from 'axios';
 
 @Injectable()
 export class WhatsAppService {
-    private client: Client;
+
+    constructor(private readonly configService: ConfigService) {
+        
+    }
+    // private client: Client;
+
+    private readonly localServerUrl = `http://${this.configService.get('IP_LOCAL')}:3001/send-message`; // cambia <IP_LOCAL> por tu IP real
+
+    async sendMessage(phone: string, message: string): Promise<string> {
+        try {
+            await axios.post(this.localServerUrl, { phone, message });
+            return `Mensaje enviado a ${phone}`;
+        } catch (error) {
+            console.error(`Error al enviar mensaje a ${phone}:`, error.message);
+            throw new Error(`No se pudo enviar el mensaje a ${phone}`);
+        }
+    }
 
     // async onModuleInit() {
     //     this.client = new Client({
@@ -27,9 +45,9 @@ export class WhatsAppService {
     //     await this.client.initialize();
     // }
 
-    async sendMessage(phone: string, message: string): Promise<string> {
-        const chatId = `${phone}@c.us`;
-        await this.client.sendMessage(chatId, message);
-        return `Mensaje enviado a ${phone}`;
-    }
+    // async sendMessage(phone: string, message: string): Promise<string> {
+    //     const chatId = `${phone}@c.us`;
+    //     await this.client.sendMessage(chatId, message);
+    //     return `Mensaje enviado a ${phone}`;
+    // }
 }
