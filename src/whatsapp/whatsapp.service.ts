6dd5094efcf7b85b089112f1945ef3a3c5913d53
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-08-18 00:27:23 -0400

    clean code whatsapp service

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -1,46 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-// import { Client, LocalAuth } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
 import { badResponse, baseResponse, DTOBaseResponse } from 'src/dto/base.dto';
-// import axios from 'axios';
-// import { badResponse } from 'src/dto/base.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { Client, LocalAuth } from 'whatsapp-web.js';
 
 @Injectable()
 export class WhatsAppService {
 
-    constructor(private readonly prismaService: PrismaService) {
-
-    }
     private client: Client;
-
-    // private readonly localServerUrl = `${this.configService.get<string>('IP_LOCAL')}/send-message`; // cambia <IP_LOCAL> por tu IP real
-
-    // async sendMessage(phone: string, message: string) {
-    //     const getUrlWhatsApp = await this.prismaService.settings.findFirst({ where: { name: 'whatsApp' } })
-
-    //     if (!getUrlWhatsApp || getUrlWhatsApp.value.trim() == '') {
-    //         badResponse.message = 'No se encontró una url para whatsApp.'
-    //         return badResponse;
-    //     }
-
-    //     try {
-    //         await axios.post(getUrlWhatsApp.value, { phone, message });
-    //         return `Mensaje enviado a ${phone}`;
-    //     } catch (error) {
-    //         console.error(`Error al enviar mensaje a ${phone}:`, error.message);
-    //         throw new Error(`No se pudo enviar el mensaje a ${phone}`);
-    //     }
-    // }
 
     async onModuleInit() {
         this.client = new Client({
             authStrategy: new LocalAuth(),
             puppeteer: {
-                // headless: true,
-                // args: ['--no-sandbox', '--disable-setuid-sandbox'],
                 headless: true,
                 args: [
                     '--no-sandbox',
